fd964c2fdd4d915062679547a71765e9dcd582d0
Pierre Segalen <pierre.segalen@gmail.com>
2020-10-13 14:24:31 +0200

    Assets - End of third episode (home screen, writing text)

--- RetroGamingBoy/assets/buildAssets.sh
+++ RetroGamingBoy/assets/buildAssets.sh
@@ -14,3 +14,24 @@
     ../src/splashscreen.tileset.png \
     --name=SPLASH_TM \
     ./splash.png
+
+./img2gb.exe tileset \
+    --output-c-file=../src/logo_rgb.tileset.c \
+    --output-header-file=../src/logo_rgb.tileset.h \
+    --output-image=../src/logo_rgb.tileset.png \
+    --deduplicate \
+    --name=LOGO_RGB_TS \
+    ./logo/rgb_gb.v2.png
+
+./img2gb.exe tilemap \
+    --output-c-file=../src/logo_rgb.tilemap.c \
+    --output-header-file=../src/logo_rgb.tilemap.h \
+    ../src/logo_rgb.tileset.png \
+    --name=LOGO_RGB_TM \
+    ./logo/rgb_gb.v2.png
+
+./img2gb.exe tileset \
+    --output-c-file=../src/font.tileset.c \
+    --output-header-file=../src/font.tileset.h \
+    --name=FONT_TS \
+    ./font.png
